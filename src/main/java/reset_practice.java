20eca5d3457d2d0d2e1a66e9262f5aee794ef9a8
Maria Serobabina <mariaserobabina@gmail.com>
2021-05-25 00:46:43 -0400

    adding changes using "soft" version

---
+++ src/main/java/reset_practice.java
@@ -0,0 +1,6 @@
+public class reset_practice {
+
+    //work 4 is done
+    //work 5 is done after resetting hard way
+
+}
--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -3,5 +3,6 @@ public class Hello {
         System.out.println(" Hello World");
         //enhancing hello
         //work1
+        //work2
     }
 }
